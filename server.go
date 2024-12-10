8c85a0e80bcd454c8cc13de8e5d74970fd944b58
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2024-12-10 09:11:33 +0530

    Improving readablitiy and security

--- server.go
+++ server.go
@@ -5,10 +5,22 @@ import (
     "log"
     "net/http"
 )
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+    if r.url.Path != "/hello"{
+        http.Error(w,"404 not found",http.StatusNotFound)
+        return
+    }
+
+    if r.Method !="GET"{
+        http.Error(w,"Method is not allowed",http.StatusMethodNotAllowed)
+        return
+    }
+
+    fmt.Fprintf(w, "Hello")
+}
 func main() {
-    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w,"Hello")
-    })
+    http.HandleFunc("/hello",helloHandler)
     fmt.Printf("Starting server at port 8084\n")
     if err := http.ListenAndServe(":8084",nil);err!=nil{
         log.Fatal(err)
