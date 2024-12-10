d654532adefd2007963e6c76007468388ec8700e
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2024-12-10 09:16:41 +0530

    Started a Static Web Server

--- server.go
+++ server.go
@@ -6,21 +6,11 @@ import (
     "net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request){
-    if r.url.Path != "/hello"{
-        http.Error(w,"404 not found",http.StatusNotFound)
-        return
-    }
 
-    if r.Method !="GET"{
-        http.Error(w,"Method is not allowed",http.StatusMethodNotAllowed)
-        return
-    }
-
-    fmt.Fprintf(w, "Hello")
-}
 func main() {
-    http.HandleFunc("/hello",helloHandler)
+    fileServer :=http.FileServer(http.Dir("./static"))
+    http.Handle("/",fileServer)
+
     fmt.Printf("Starting server at port 8084\n")
     if err := http.ListenAndServe(":8084",nil);err!=nil{
         log.Fatal(err)
